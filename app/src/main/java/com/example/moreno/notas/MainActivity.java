648a47d2ff90efd2633c801cff7dc5da96b12910
Ali <mamr.213@gmail.com>
2016-11-13 15:56:39 -0700

    Creado content provider

--- app/src/main/java/com/example/moreno/notas/MainActivity.java
+++ app/src/main/java/com/example/moreno/notas/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.moreno.notas;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT,"New note");
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,values);
+        Log.d("MainActivity","Inserted note" + noteUri.getLastPathSegment() );
     }
 
 }
