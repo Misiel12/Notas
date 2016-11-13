12bbd0a071ed1419023952f9ae22a072838e30ea
Ali <mamr.213@gmail.com>
2016-11-13 15:20:58 -0700

    Creado Helper de base de datos

---
+++ app/src/main/java/com/example/moreno/notas/DBOpenHelper.java
@@ -0,0 +1,46 @@
+package com.example.moreno.notas;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Moreno on 13/11/2016.
+ */
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+    //Constantes para el nombre de la base de datos y la version
+    private static final String DATABASE_NAME = "notes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //Constantes para identificar tablas y columnas
+    public static final String TABLE_NOTES = "notes";
+    public static final String NOTE_ID = "_id";
+    public static final String NOTE_TEXT = "noteText";
+    public static final String NOTE_CREATED = "noteCreated";
+
+    //SQL sentencia para crear tabla
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NOTES + " (" +
+                    NOTE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    NOTE_TEXT + " TEXT, " +
+                    NOTE_CREATED + " TEXT default CURRENT_TIMESTAMP" +
+                    ")";
+
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+            db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTES );
+        onCreate(db);
+    }
+}
