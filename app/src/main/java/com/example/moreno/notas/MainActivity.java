d32f4af930f1a1cfde9ae0ae98a04eb8d6fcba22
Ali <mamr.213@gmail.com>
2016-11-14 10:21:21 -0700

    se creo un cursor adapter personalizado

--- app/src/main/java/com/example/moreno/notas/MainActivity.java
+++ app/src/main/java/com/example/moreno/notas/MainActivity.java
@@ -35,10 +35,10 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 
 
         String[] from = {DBOpenHelper.NOTE_TEXT};
-        int[] to = {android.R.id.text1};
+        int[] to = {R.id.tvNote};
 
         cursorAdapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1, null, from, to, 0);
+                R.layout.note_list_item, null, from, to, 0);
         ListView List = (ListView) findViewById(R.id.list_view_1);
 
        List.setAdapter(cursorAdapter);
