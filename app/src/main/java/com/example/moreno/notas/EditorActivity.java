59f441f6dc4fece3b1c500c816bbab976a1f1e93
Ali <mamr.213@gmail.com>
2016-11-14 12:31:35 -0700

    agregado el guardar y actualizar notas

--- app/src/main/java/com/example/moreno/notas/EditorActivity.java
+++ app/src/main/java/com/example/moreno/notas/EditorActivity.java
@@ -2,16 +2,20 @@ package com.example.moreno.notas;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditorActivity extends AppCompatActivity {
     private String action;
     private EditText editor;
+    private String noteFilter;
+    private String oldText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,16 @@ public class EditorActivity extends AppCompatActivity {
         if (uri == null) {
             action = Intent.ACTION_INSERT;
             setTitle("Nueva Nota");
+        } else {
+            action = Intent.ACTION_EDIT;
+            noteFilter = DBOpenHelper.NOTE_ID + "=" + uri.getLastPathSegment();
+
+            Cursor cursor = getContentResolver().query(uri,
+                    DBOpenHelper.ALL_COLUMNS, noteFilter, null, null);
+            cursor.moveToFirst();
+            oldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+            editor.setText(oldText);
+            editor.requestFocus();
         }
     }
 
@@ -61,6 +75,14 @@ public class EditorActivity extends AppCompatActivity {
                     setResult(RESULT_CANCELED);
                 } else {
                     insertNote(newText);
+                } break;
+            case Intent.ACTION_EDIT:
+                if (newText.length() == 0) {
+//                    deleteNote();
+                } else if (oldText.equals(newText)) {
+                    setResult(RESULT_CANCELED);
+                } else {
+                    updateNote(newText);
                 }
         }
         finish();
@@ -70,6 +92,14 @@ public class EditorActivity extends AppCompatActivity {
         ContentValues values = new ContentValues();
         values.put(DBOpenHelper.NOTE_TEXT, noteText);
         getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        setResult(RESULT_OK);
+    }
+
+    private void updateNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().update(NotesProvider.CONTENT_URI, values, noteFilter, null);
+        Toast.makeText(this, R.string.nota_actualizada, Toast.LENGTH_SHORT).show();
         setResult(RESULT_OK);
     }
 
